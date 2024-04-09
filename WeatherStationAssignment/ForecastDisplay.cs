75c53d7b0b3a25663c2f3844f77245626d902b9c
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-09 15:07:08 +0000

    ForecastDisplay

---
+++ WeatherStationAssignment/ForecastDisplay.cs
@@ -0,0 +1,36 @@
+namespace WeatherStation.Weather;
+using Weather.Interfaces;
+
+/// <summary>
+/// Another observer and decorator.
+/// </summary>
+public class ForecastDisplay : IDisplay<string>
+{
+    private double output;
+    public void Display(String param){
+        if(double.TryParse(param, out output))
+        {
+            switch(output){
+            case <= -20.0: 
+                Console.WriteLine("{0}°C: Raining! ", output);
+                break;
+
+            case <= -5.0: 
+                 Console.WriteLine("{0}°C: Rainy day! ", output);
+                 break;
+            case <= 10.0: 
+                 Console.WriteLine("{0}°C: Sunny and partly cloudy! ", output);
+                 break;
+            case <= 25.0: 
+                 Console.WriteLine("{0}°C: Sunny day all week! ", output);
+                 break;
+
+            case <=35.0: 
+                 Console.WriteLine("{0}°C: Walm! ", output);
+                 break;
+            default:
+                throw new Exception("wrong input");
+        };
+        }
+    }
+}
