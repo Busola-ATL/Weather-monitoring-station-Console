64b7d28603a1f4864175e01cf64cab23de2f481d
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-09 15:04:07 +0000

    StatisticsDisplay added

---
+++ WeatherStationAssignment/StatisticsDisplay.cs
@@ -0,0 +1,27 @@
+namespace WeatherStation.Weather;
+using Weather.Interfaces;
+
+/// <summary>
+/// Another observer. It displays weather statistics.
+/// </summary>
+
+
+
+public class StatisticsDisplay : IDisplay<string>
+{
+    double solution;
+    IList<double> sumParameters = new List<double>();
+    public void Display(string parameter)
+    {
+        if(double.TryParse(parameter, out solution))
+            sumParameters.Add(solution);
+        else
+            throw new Exception("String can't be passed to double");
+
+        Console.WriteLine("Average temperature: "+ sumParameters.Average());
+        Console.WriteLine("Maximum temperature: "+ sumParameters.Max());
+        Console.WriteLine("Minimum temperature: "+ sumParameters.Min());
+
+    }
+    
+}
