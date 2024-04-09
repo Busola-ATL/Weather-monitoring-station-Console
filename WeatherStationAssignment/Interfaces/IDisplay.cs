69d3204beeefa71474588bc3405ac59de1273f65
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-09 14:45:31 +0000

    Interfece for the program created

---
+++ WeatherStationAssignment/Interfaces/IDisplay.cs
@@ -0,0 +1,9 @@
+namespace WeatherStation.Weather.Interfaces;
+/// <summary>
+/// IDisplay: A generic interface that will be implemented by all observer classes. 
+/// </summary>
+/// <typeparam name="T">Any type goes with</typeparam>
+public interface IDisplay<T>
+{
+    void Display(T? parameter);
+}
